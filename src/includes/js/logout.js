6825d66b5a7f6300d428e56ef8c976e0e19bc215
ddavisgraphics <ddavisgraphics@gmail.com>
2016-01-18 10:39:28 -0500

    logout that fires the save session stuff

--- src/includes/js/logout.js
+++ src/includes/js/logout.js
@@ -1,3 +1,11 @@
 $(function(){
-
+    window.onbeforeunload = closeSession;
+    function closeSession(){
+        $.ajax({
+            url   : "/login/logout/",
+            type  : "GET",
+            async : false;
+        });
+        return "Your session progress has been saved!";
+    }
 });
